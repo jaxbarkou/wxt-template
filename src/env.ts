e4877980a67f3cb2d731e7487c800dccb3e8e465
leeamijo <leeamijo@gmail.com>
2025-08-22 11:06:09 +0800

    feat: update host

--- src/env.ts
+++ src/env.ts
@@ -3,7 +3,8 @@ export const env = {
     // NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/api",
     // NEXT_PUBLIC_STATIC_WEBSITE_ONLY: process.env.NEXT_PUBLIC_STATIC_WEBSITE_ONLY === "true",
     // GITHUB_OAUTH_TOKEN: process.env.GITHUB_OAUTH_TOKEN || ''
-    NEXT_PUBLIC_API_URL: "http://localhost:8000/api",
+    NEXT_PUBLIC_API_URL: "http://34.142.207.10:8000/api",
+    // NEXT_PUBLIC_API_URL: "http://localhost:8000/api",
     NEXT_PUBLIC_STATIC_WEBSITE_ONLY: false,
     GITHUB_OAUTH_TOKEN: '',
     LANGUAGE: "en",
